396207a0551ca5397d977cbf5622bb976d894a27
OfflineHTTPServer <125279720+OfflineHTTPServer@users.noreply.github.com>
2023-08-02 10:28:43 +0200

    Add Installer

---
+++ PowerClicker_Installer.py
@@ -0,0 +1,106 @@
+import urllib.request
+import os
+
+DirName: str = "PowerClicker"
+ExecutableName: str = "PowerClicker.exe"
+LicenseFile_Name: str = "LICENSES.txt"
+LicenseFile_Text: str = """
+PowerClicker - MIT License
+
+Copyright (c) 2023 Arman Behravan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+Third-party libraries used by PowerClicker:
+
+imgui - MIT License
+https://github.com/ocornut/imgui/blob/master/LICENSE.txt
+
+The MIT License (MIT)
+
+Copyright (c) 2014-2023 Omar Cornut
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+glfw - zlib/libpng License
+https://www.glfw.org/license
+
+zlib/libpng license
+
+Copyright (c) 2002-2006 Marcus Geelnard
+
+Copyright (c) 2006-2019 Camilla Löwy
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would
+   be appreciated but is not required.
+
+2. Altered source versions must be plainly marked as such, and must not
+   be misrepresented as being the original software.
+
+3. This notice may not be removed or altered from any source
+   distribution.
+"""
+
+def InstallerStatusMessage(x: str) -> str:
+    print(f"Installer: {x}")
+
+def main() -> None:
+    
+    os.makedirs(DirName)
+    InstallerStatusMessage("PowerClicker directory created.")
+
+    with urllib.request.urlopen("https://github.com/OfflineHTTPServer/PowerClicker/raw/main/bin/PowerClicker.exe") as response:
+        with open(f"{DirName}/{ExecutableName}", "wb") as stream:
+            stream.write(response.read())
+            InstallerStatusMessage("Executable created.")
+
+    with open(f"{DirName}/{LicenseFile_Name}", "w") as stream:
+        stream.write(LicenseFile_Text)
+        InstallerStatusMessage("License file created.")
+
+if __name__ == '__main__':
+    main()
